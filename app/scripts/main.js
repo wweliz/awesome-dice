bcf948a1f85645bee9ab16cffd46857f8ac63cd8
Whitney Williams <wweliz@gmail.com>
2014-05-24 14:02:15 -0400

    final changes to input limits

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,39 +2,36 @@
 
 // Die Constructor
 function Game (sides,howMany) {
-  this.diceSides = parseInt(sides);
-  this.howMany = parseInt(howMany);
-  this.roll = generateArray(howMany, 1, sides);
+	this.diceSides = parseInt(sides);
+	this.howMany = parseInt(howMany);
+	this.roll = generateArray(howMany, 1, sides);
 }
 
 // Roll Dice Button
 $('.roll-dice').click(function(){
-  $('.dice-container').empty();
+	$('.dice-container').empty();
 	var howMany = $('.input-how-many-dice').val();
-  var sides = $('.input-number-of-sides').val();
+	var sides = $('.input-number-of-sides').val();
 	var gameDie = new Game(sides,howMany);
-	;
 });
 
 // Generate Individual Random Number
 function randomNumber(min,max) {
-  return (Math.round((max-min) * Math.random() + min));
+	return (Math.round((max-min) * Math.random() + min));
 }
 
 // Generate Array of Random Numbers
 function generateArray(numDice,minDiceSides,maxDiceSides) {
 	var randomArray = [];
-  for (var dice=0; dice<numDice; dice++) {
-    randomArray[dice] = randomNumber(minDiceSides,maxDiceSides);
+	for (var dice=0; dice<numDice; dice++) {
+		randomArray[dice] = randomNumber(minDiceSides,maxDiceSides);
 }
-
-// not sure if we need the below function -- will need to change die-box name
 
 // Reset Button Function
 $('.reset').click(function() {
-  $('.input-number-of-sides').val('');
-  $('.input-how-many-dice').val('');
-  $('.dice-container').empty();
+	$('.input-number-of-sides').val('');
+	$('.input-how-many-dice').val('');
+	$('.dice-container').empty();
 });
 
 // not sure if we need the below function either
--- test/spec/test.js
+++ test/spec/test.js
@@ -7,53 +7,53 @@ describe('Reset Button', function() {
 	$('.reset').click();
 
 	it('should set input-number-of-sides input field to empty', function() {
- 	expect($('.input-number-of-sides').val()).to.equal();
+	expect($('.input-number-of-sides').val()).to.equal();
 
- 	})
+	});
 
 	it('should set input-how-many-dice input field to empty', function() {
 		expect($('.input-how-many-dice').val()).to.equal();
-	})
+	});
 
-})
+});
 
 // Test reset button to clear dice-container
  describe('Reset Button', function() {
- 	
- 	$('.box1').val(34);
-  
- 	$('.reset').click();
- 	
+	
+	$('.box1').val(34);
+	
+	$('.reset').click();
+	
 	it('should set dice-container val to empty', function() {
- 		expect($('.dice-container').val()).to.equal();
- 	})
- })
+		expect($('.dice-container').val()).to.equal();
+	});
+ });
 
 //Random Array Generator
 describe('Random Array Generator', function(){
 
-  it('Should generate a random array of results for the die depending on Number Of Dice passed in variable and contain the Max property.', function(){
+	it('Should generate a random array of results for the die depending on Number Of Dice passed in variable and contain the Max property.', function(){
 
-    function generateArray(numDice,minDiceSides,maxDiceSides) {
-      var randomArray = [];
-        for (var dice=0; dice<numDice; dice++) {
-        randomArray[dice] = randomNumber(minDiceSides,maxDiceSides);
-        expect(randomNumber).to.have.property('numDice');
-        }
-    }
-  })
-})
+		function generateArray(numDice,minDiceSides,maxDiceSides) {
+			var randomArray = [];
+				for (var dice=0; dice<numDice; dice++) {
+				randomArray[dice] = randomNumber(minDiceSides,maxDiceSides);
+				expect(randomNumber).to.have.property('numDice');
+				}
+		}
+	});
+});
 
 describe('Random Dice Side Generator', function(){
 
-  it('Should generate a random result for the die depending on maxium number of sides passed in variable and contain the Max property.', function(){
+	it('Should generate a random result for the die depending on maxium number of sides passed in variable and contain the Max property.', function(){
 
-    function randomNumber(min,max) {
-      return (Math.round((max-min) * Math.random() + min));
-      expect(randomNumber).to.have.property('max');
-    }
-  })
-})
+		function randomNumber(min,max) {
+			return (Math.round((max-min) * Math.random() + min));
+			expect(randomNumber).to.have.property('max');
+		}
+	});
+});
 
 // Die Constructor
 describe('Game Constructor', function(){
